bf36eaf44a25e466bdd5c88aa4d132100ca51e6f
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-02-22 20:44:12 -0300

    con inicio modificado

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <PeliculaState>
-      <Router>
+      <Router basename='/cine-renzi'>
         <Header />
         <Routes>
           <Route exact path='/' element={<Inicio />} />
